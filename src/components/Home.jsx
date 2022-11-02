9cb108426c903dc9c32d7d942204488d25468be5
Shubh <shubh2268@gmail.com>
2022-11-02 09:01:48 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Frontend Developer</h2>
                 <p className='text-gray-500 py-4 max-w-md'>I have 8 years of experience building and designing software. Currently, I love to work on web application using technology like ReactJs, Tailwind, NextJs and GraphQL.</p>
                 <div>
                     <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
