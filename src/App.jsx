8b89ed647af55c870e3b80043367b48c809f9fe6
Shubh <shubh2268@gmail.com>
2024-03-01 17:25:33 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
