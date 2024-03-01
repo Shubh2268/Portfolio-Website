106eaf80ca4166cac03d536444aee7977f1879e3
Shubh <shubh2268@gmail.com>
2024-03-01 17:21:17 +0530

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LiaTimesSolid } from 'react-icons/lia';
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 const Navbar = () => {
+
+    const [navigation, setNavigation] = useState(false);
 
     const links = [
         {
@@ -45,6 +49,43 @@ const Navbar = () => {
                         ))
                     }
                 </ul>
+
+                {/* burger icon */}
+                <div className='sm:hidden'>
+                    <div className='cursor-pointer'>
+                        <HiOutlineMenuAlt2 onClick={() => setNavigation(true)} size={25} />
+                    </div>
+                </div>
+            </div>
+
+            {/* mobile view */}
+            <div className={navigation ? 'md:hidden fixed left-0 top-0 w-full h-full bg-gray-900/70 backdrop-blur' : ''}>
+                <div className={navigation ? 'fixed left-0 top-0 w-4/5 h-full bg-gray-900 text-white p-10 ease-in duration-500' : 'fixed top-0 left-[-100%] h-full p-10 duration-500 ease-in'}>
+                    <div>
+                        <div className='flex w-full items-center justify-between'>
+                            <h2 className='text-2xl font-signature capitalize cursor-pointer'>Shubh</h2>
+                            <div className='p-3 cursor-pointer'>
+                                <LiaTimesSolid onClick={() => setNavigation(false)} size={25} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='mt-10 flex flex-col h-fit gap-12'>
+
+                        <ul className='capitalize'>
+                            {
+                                links.map(({ id, link }) => (
+                                    <li key={id} className='py-2 text-lg text-center font-medium cursor-pointer'>
+                                        <a onClick={() => setNavigation(false)} offset={-75}>
+                                            {link}
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+
+                    </div>
+                </div>
             </div>
 
         </div>
