76062a35f0fde2879691128af0858895a5ab233e
Shubh <shubh2268@gmail.com>
2024-03-02 16:45:44 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Experience from './components/Experience';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Experience />
     </div>
   )
 }
