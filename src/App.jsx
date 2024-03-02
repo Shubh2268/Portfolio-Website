a027f4c49b4d7d380100b11221caab5fb9274faf
Shubh <shubh2268@gmail.com>
2024-03-02 16:48:24 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Experience from './components/Experience';
+import Contact from './components/Contact';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Portfolio />
       <Experience />
+      <Contact />
     </div>
   )
 }
