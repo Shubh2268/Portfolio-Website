e52bd701f4ceb6f1ff93a260e363445235b3ac7c
Shubh <shubh2268@gmail.com>
2024-03-14 13:26:02 +0530

    --

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,13 @@ const About = () => {
 
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
 
-                <div className='pb-8 pt-0'>
-                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                <div className='flex justify-center md:justify-start items-center pb-8 pt-0'>
+                    <p className='text-2xl sm:text-3xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
 
-                <p className='text-xl mt-10'>Organized and dependable candidate, successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals.</p>
+                <p className='text-base md:text-xl mt-5'>Organized and dependable candidate, successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals.</p>
                 <br />
-                <p className='text-xl'>Enthusiastic Frontend Developer, eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of developement and training in Relevel. Motivated to learn, grow and excel in Software Industry.</p>
+                <p className='text-base md:text-xl'>Enthusiastic Frontend Developer, eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of developement and training in Relevel. Motivated to learn, grow and excel in Software Industry.</p>
 
             </div>
 
