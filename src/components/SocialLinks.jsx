30664d28775aa3ea27c07c3a716b84b0af9bff2d
Shubh <shubh2268@gmail.com>
2024-04-03 13:20:17 +0530

    --

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: 'Resume.pdf',
+            href: '/Resume.pdf',
             style: 'rounded-br-md',
             download: true
         }
