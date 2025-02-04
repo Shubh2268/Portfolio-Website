f2b6ac81a4a8878882e24677367cba6ed35454bc
Shubh <shubh2268@gmail.com>
2025-02-04 20:00:06 +0530

    --

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
                 <div className='flex flex-col items-center md:items-start justify-center h-full'>
                     <h2 className='text-3xl sm:text-4xl lg:text-7xl font-bold text-white text-center md:text-start'>I'm a Frontend Developer</h2>
-                    <p className='text-gray-300 py-5 max-w-md text-center md:text-start text-sm sm:text-base w-full'>I'm 23 years old. I have a great passion for building and designing software. Currently, I love to work on web application using technology like ReactJs, Tailwind, and Bootstrap.</p>
+                    <p className='text-gray-300 py-5 max-w-md text-center md:text-start text-sm sm:text-base w-full'>I am proficient in ReactJS, JavaScript, and other modern frontend tools. I enjoy building clean, responsive, and user-friendly web applications while continuously learning and improving. With a strong eye for detail and a problem-solving mindset, I strive to create seamless digital experiences.</p>
                     <div>
                         <Link to='portfolio' smooth duration={500} className='group text-white font-semibold w-fit px-6 py-3 my-2 flex items-center text-sm sm:text-base rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                             Portfolio <span className='group-hover:rotate-90 duration-300'> <HiArrowNarrowRight size={20} className='ml-1' /> </span>
@@ -20,7 +20,7 @@ const Home = () => {
                 </div>
 
                 <div className='py-10 sm:pt-0'>
-                    <img src={myImage} alt='my profile' className='rounded-full border-2 border-gray-400 mx-auto w-2/3 sm:w-3/4 lg:w-full' />
+                    <img src={myImage} alt='my profile' className='rounded-full border-2 border-cyan-600 mx-auto w-2/3 sm:w-3/4 lg:w-full shadow-md shadow-cyan-600' />
                 </div>
 
             </div>
