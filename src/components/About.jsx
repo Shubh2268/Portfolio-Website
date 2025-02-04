3e0960cddd406aa47f75cf297e1719071c7737c9
Shubh <shubh2268@gmail.com>
2025-02-04 20:40:27 +0530

    --

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,14 +10,22 @@ const About = () => {
                     <p className='text-2xl sm:text-3xl lg:text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
 
-                <p className='text-base sm:text-lg lg:text-xl mt-5'>Organized and dependable candidate, successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals.</p>
+                <p className='text-base md:text-lg mt-5'>
+                    I'm a self-taught Frontend Developer, with a strong foundation in ReactJS, JavaScript, and modern frontend tools. I have a passion for creating responsive, user-friendly web applications that blend aesthetics with functionality. My focus is on clean code, performance optimization, and delivering seamless digital experiences.
+                </p>
                 <br />
-                <p className='text-base sm:text-lg lg:text-xl'>Enthusiastic Frontend Developer, eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of developement and training in Relevel. Motivated to learn, grow and excel in Software Industry.</p>
+                <p className='text-base md:text-lg'>
+                    With a problem-solving mindset and attention to detail, I enjoy transforming designs into interactive, pixel-perfect interfaces. I stay updated with the latest industry trends and continuously sharpen my skills to build intuitive and efficient applications. I am organized, adaptable, and always eager to learn. Whether it's collaborating with a team or working independently, I take initiative and embrace challenges as opportunities to grow.
+                </p>
+                <br />
+                <p className='text-base md:text-lg'>
+                    Currently, I'm seeking an entry-level Frontend Developer role, where I can contribute my skills, gain real-world experience, and grow alongside a dynamic team.🚀
+                </p>
 
             </div>
 
         </div>
-    )
+    );
 }
 
 export default About;
